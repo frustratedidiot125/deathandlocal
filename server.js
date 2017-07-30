feecaa46cc140e744dc2dbc113ec7cd32d2602d0
frustratedidiot124 <30528649+frustratedidiot124@users.noreply.github.com>
2017-07-30 12:19:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ alexaApp.express({expressApp: expressApp, router: express.Router(), debug: false
 
 
 alexaApp.launch( (request, response) => {
-  let content = `Welcome to Common Quotes Quiz! I'll give you a quote and you tell me who you think it's from! Just say give me a quote, or atop to exit!`;
+  let content = `Welcome to Common Quotes Quiz! I'll give you a quote and you tell me who you think it's from! Just say give me a quote to start, or stop to exit!`;
 
   response.card('Welcome', content);
   response.say(content);
@@ -26,11 +26,16 @@ alexaApp.launch( (request, response) => {
 
 alexaApp.intent("Givemeaquote", {
     slots: {},
-    utterances: ['Give me a quote', 'Try again', 'Hit me', 'Quote me', 'Yes']
+    utterances: ['Give me a quote', 'Try again', 'Hit me', 'Quote me', 'Yes', 'Start']
   },
   function (request, response) {
   var randomQuoteskey = Object.keys(quotesall)[Math.floor(Math.random()*Object.keys(quotesall).length)]; //randomQuoteskey is the quote itself
    let speaker = quotesall[randomQuoteskey]; //quote itself serves are key for value speaker of quote
+  let contentq = 'Who said. " + randomQuoteskey
+  // **888 --WE NEED TO FIGURE OYT SESSION VARIABLES IE ALEXAAPP SET SESSION OR .SET SESSION VAR OR RES.
+  
+  
+  
   
   
  //here goes phone anc other var
@@ -38,25 +43,26 @@ alexaApp.intent("Givemeaquote", {
 
   //  let selabrev = newcountries[givencountry];
 console.log('speaker:', speaker);
-if (selabrev) {
 
-    let callingcode = phonecodes[selabrev];
+  // could be useful:  if (selabrev) {
+
+   // let callingcode = phonecodes[selabrev];
 
     
-console.log('callingcode:', callingcode);
+// console.log('callingcode:', callingcode);
 
-    if (callingcode) {
-      response.say(`The international calling code for ${givencountry} is <say-as interpret-as='spell-out'>${callingcode}</say-as>`);
-      response.shouldEndSession(true);
+    //if (callingcode) {
+    //  response.say(`The international calling code for ${givencountry} is <say-as interpret-as='spell-out'>${callingcode}</say-as>`);
+     // response.shouldEndSession(true);
     
-   } else {
-      response.say(`I\'m sorry, I do not have a calling code on file for ${givencountry}. Though I do recognize the country, they might not have international calling capabilities yet. `);
-  response.shouldEndSession(true);
-}
-     } else {
-      response.say(`I\'m sorry, I do not recognize ${givencountry}. Speaking literally, of course, not diplomatically.`);
- response.shouldEndSession(true);
-      }
+//   } else {
+  //    response.say(`I\'m sorry, I do not have a calling code on file for ${givencountry}. Though I do recognize the country, they might not have international calling capabilities yet. `);
+  //response.shouldEndSession(true);
+//}
+   //  } else {
+     // response.say(`I\'m sorry, I do not recognize ${givencountry}. Speaking literally, of course, not diplomatically.`);
+// response.shouldEndSession(true);
+      //}
   }
   
   
