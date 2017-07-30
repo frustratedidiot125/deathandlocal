83a72440bcac5a2a7809a396dcfb21f5c5661201
frustratedidiot124 <30528649+frustratedidiot124@users.noreply.github.com>
2017-07-30 18:11:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ let bodyParser = require('body-parser');
 let alexa = require('alexa-app');
 let invert = require('lodash.invert');
 let keyBy = require('lodash.keyby');
-//let phonecodes = require("./phone.json");
-//let countries = require("./names.json");
-//let quotesall = require("./quotes1.json");
-let quotesall = require("./quotesfbig.json");
+
+let statesages = require("./ages2.json");
 
 
 let alexaApp = new alexa.app('alexa');
@@ -16,80 +14,35 @@ alexaApp.express({expressApp: expressApp, router: express.Router(), debug: false
 
 
 alexaApp.launch( (request, response) => {
-  let content = `Hi there! Give me a famous name and I'll read you one of their quotes! You can also say random quote, for a random quote, or stop to exit.`;
+  let content = `Hi there! Name a state and  I'll  tell you the minimum driving age there.`;
 
   response.card('Welcome', content);
   response.say(content);
-  response.reprompt('I\m waiting. Just say a notable name or say random quote, or say stop to exit.')
+  response.reprompt('Name a state or say stop to exit.')
   response.shouldEndSession(false);
 });
 
-alexaApp.intent("Randomquote", {
-    slots: {},
-    utterances: ['Give me a quote', 'random', 'random quote', 'another', 'hit me again']
-  },
-  function (request, response) {
-  var randomQuoteskey = Object.keys(quotesall)[Math.floor(Math.random()*Object.keys(quotesall).length)]; //randomQuoteskey is the quote itself
-   let speaker = quotesall[randomQuoteskey]; //quote itself serves are key for value speaker of quote
-  let content =  speaker + " said " + randomQuoteskey;
-  //let prompt = "Would you like to hear more? You can say another, or stop to exit."
-  // **888 --WE NEED TO FIGURE OYT SESSION VARIABLES IE ALEXAAPP SET SESSION OR .SET SESSION VAR OR RES.
-  response.card(speaker, randomQuoteskey);
-  response.say(content);
-  response.shouldEndSession(true);  
-  console.log(content);
-  
-  
-  
- //here goes phone anc other var
-//    let givencountry = request.slot('Country');
+ 
 
-  //  let selabrev = newcountries[givencountry];
-//console.log('speaker:', speaker);
-
-  // could be useful:  if (selabrev) {
-
-   // let callingcode = phonecodes[selabrev];
-
-    
-// console.log('callingcode:', callingcode);
-
-    //if (callingcode) {
-    //  response.say(`The international calling code for ${givencountry} is <say-as interpret-as='spell-out'>${callingcode}</say-as>`);
-     // response.shouldEndSession(true);
-    
-//   } else {
-  //    response.say(`I\'m sorry, I do not have a calling code on file for ${givencountry}. Though I do recognize the country, they might not have international calling capabilities yet. `);
-  //response.shouldEndSession(true);
-//}
-   //  } else {
-     // response.say(`I\'m sorry, I do not recognize ${givencountry}. Speaking literally, of course, not diplomatically.`);
-// response.shouldEndSession(true);
-      //}
-  }
-  
-  
-);
-
-alexaApp.intent("Namedquote", {
-    slots: {Famousname: 'AMAZON.Name'}, //fullname??
-    utterances: ['Give me a {-|Famousname} quote', '{-|Famousname}']
+alexaApp.intent("Startstate", {
+    slots: {State: 'AMAZON.US_STATE'}, //fullname??
+    utterances: ['what\'s the driving bage age in {-|State}', '{-|State}']
   },
            
     function (request, response) {            
-  var reqdname = request.slot('Famousname');
-  var quotesome = invert(quotesall);
-    var selectednq = quotesome[reqdname];
+  var reqdname = request.slot('State');
+ 
+    var selectednq = statesages[reqdname];
   
   console.log('selectednq:', selectednq)
   if (selectednq){
-      let content = reqdname + " once said. " + selectednq;
+      let content = "The minimum driving age in " + reqdname + " is " + selectednq + " years old. with a learner's permit.";
     console.log('content:', content);  
     response.card(reqdname, selectednq);
       response.say(content);
       response.shouldEndSession(true);
     } else {
-      response.say("I'm sorry, I can't seem to find any " + reqdname + " quotes at the moment.");
+      response.say("I'm sorry, I only know US states. I don't think " + reqdname + " is a valid US state.");
       response.shouldEndSession(true);
     }
 }
@@ -102,8 +55,8 @@ alexaApp.intent("AMAZON.HelpIntent", {
   //              ]
 //  },
   function(request, response) {
-  var HELP_MESSAGE = "Give me a famous name and I'll read you one of their quotes! You can also say random, for a random quote, or stop to exit.";
-var HELP_REPROMPT = "Please give me a name, say random, or say stop to exit.";
+  var HELP_MESSAGE = "Name a US state and  I'll try to tell you the minimum driving age there, or You can also say stop to exit. `;
+var HELP_REPROMPT = "Name a state or say stop to exit.";
  response.say(HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false);
   }
  );
